04efe4d2bd8b5fe2f5cb103563bef8ac621defa1
neepmeat <o.o@oo.o>
2022-02-14 22:54:24 +0000

    Added items for auto-stairs.

--- src/main/java/com/neep/neepmeat/block/base/BaseSlabBlock.java
+++ src/main/java/com/neep/neepmeat/block/base/BaseSlabBlock.java
@@ -1,18 +1,22 @@
 package com.neep.neepmeat.block.base;
 
+import com.neep.neepmeat.item.BaseBlockItem;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.item.BlockItem;
 
 public class BaseSlabBlock extends SlabBlock implements NMBlock
 {
     protected String registryName;
     protected int maxStack;
+    protected BlockItem blockItem;
 
     protected BaseSlabBlock(BlockState baseBlockState, String blockName, int itemMaxStack, Settings settings)
     {
         super(settings);
         this.registryName = blockName;
         this.maxStack = itemMaxStack;
+        this.blockItem = new BaseBlockItem(this, blockName, itemMaxStack, false);
     }
 
     @Override
