c8875ffeb64ff3f01cb94bbfd958b746c53ca1fd
neepmeat <o.o@oo.o>
2022-03-23 21:00:20 +0000

    Added disassembly

--- src/main/java/com/neep/assembly/DebugItem.java
+++ src/main/java/com/neep/assembly/DebugItem.java
@@ -1,9 +1,13 @@
 package com.neep.assembly;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -29,5 +33,12 @@ public class DebugItem extends Item
             return ActionResult.SUCCESS;
         }
         return ActionResult.FAIL;
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand)
+    {
+        System.out.println(entity);
+        return ActionResult.SUCCESS;
     }
 }
