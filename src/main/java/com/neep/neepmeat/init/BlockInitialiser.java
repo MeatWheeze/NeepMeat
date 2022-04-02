15897240b3ad5788ca35efc03aa4c86c24e5e530
neepmeat <o.o@oo.o>
2022-04-02 18:45:32 +0100

    Changed block registration to use static things

--- src/main/java/com/neep/neepmeat/init/BlockInitialiser.java
+++ src/main/java/com/neep/neepmeat/init/BlockInitialiser.java
@@ -15,23 +15,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @SuppressWarnings("unused")
 public class BlockInitialiser
 {
-    public static final Map<String, NMBlock> BLOCKS = new HashMap<>();
+    public static final Map<Identifier, NMBlock> BLOCKS = new LinkedHashMap<>();
 
-    public static Block MESH_PANE;
-
-    public static Block SCAFFOLD_TRAPDOOR;
-    public static Block RUSTED_BARS;
-    public static Block RUSTY_VENT;
-    public static Block RUSTY_PANEL;
-    public static Block RUSTY_GRATE;
     public static BasePaintedBlock SMOOTH_TILE = new BasePaintedBlock("smooth_tile", FabricBlockSettings.of(Material.STONE).hardness(5.0f));
-
-    public static Block SLOPE_TEST;
 
     public static Block POLISHED_IRON_BRICKS = new BaseBuildingBlock("polished_metal_bricks", 64, true, FabricBlockSettings.of(Material.METAL).strength(5.0f).sounds(BlockSoundGroup.NETHERITE));
     public static BaseBuildingBlock RUSTED_IRON_BLOCK = new BaseBuildingBlock("polished_metal", 64, true, FabricBlockSettings.of(Material.METAL).strength(5.0f).sounds(BlockSoundGroup.NETHERITE));
@@ -48,96 +40,65 @@ public class BlockInitialiser
     public static MetalScaffoldingBlock BLUE_SCAFFOLD = new MetalScaffoldingBlock("blue_metal_scaffold", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE));
     public static MetalScaffoldingBlock YELLOW_SCAFFOLD = new MetalScaffoldingBlock("yellow_metal_scaffold", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE));
 
-    public static Block TEST;
-    public static Block PIPE;
-    public static Block TANK;
-    public static Block GLASS_TANK;
-    public static Block ITEM_BUFFER;
-    public static Block FLUID_METER;
-    public static Block FLUID_PORT;
-    public static Block SPIGOT;
+    // --- Building Blocks ---
+    public static Block RUSTY_VENT = queueBlock(new BaseColumnBlock("rusty_vent", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE).nonOpaque()));
+    public static Block MESH_PANE = queueBlock(new BasePaneBlock("mesh_panel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
+    public static Block RUSTED_BARS = queueBlock(new BasePaneBlock("rusted_bars", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
+    public static Block RUSTY_PANEL = queueBlock(new BaseBlock("rusty_panel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block RUSTY_GRATE = queueBlock(new BaseBlock("rusty_grate", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+//        GREY_SMOOTH_TILE = registerBlock(new BaseBlock("smooth_tile_grey", 64, false, FabricBlockSettings.of(Material.STONE).strength(4.0f).sounds(BlockSoundGroup.STONE)));
 
-    public static Block ITEM_DUCT;
-    public static Block IRON_PIPE;
-    public static Block COPPER_PIPE;
-    public static Block PUMP;
-    public static Block TROMMEL;
-    public static Block HEATER;
-    public static Block TROMMEL_CENTRE;
-    public static Block FLUID_DRAIN;
+    public static Block SLOPE_TEST = queueBlock(new BaseStairsBlock(CAUTION_BLOCK.getDefaultState(), "slope_test", 64, FabricBlockSettings.of(Material.METAL).nonOpaque()));
 
-    public static Block LINEAR_RAIL;
+    public static Block SCAFFOLD_TRAPDOOR = queueBlock(new ScaffoldTrapdoorBlock("rusted_metal_scaffold_trapdoor", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE).nonOpaque()));
 
-    public static Block BIG_LEVER;
+    // --- General Blocks ---
+    public static Block PIPE = queueBlock(new FluidPipeBlock("pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block IRON_PIPE = queueBlock(new FluidPipeBlock("iron_pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block COPPER_PIPE = queueBlock(new FluidPipeBlock("copper_pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block TROMMEL = queueBlock(new TrommelBlock("trommel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
+    public static Block TROMMEL_CENTRE = queueBlock(new BaseDummyBlock("trommel_centre", FabricBlockSettings.of(Material.METAL).strength(4.0f)));
+    public static Block HEATER = queueBlock(new HeaterBlock("heater", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
 
-    public static Block INTEGRATOR_EGG;
-    public static Block TANK_WALL;
+    // --- Fluid Transfer ---
+    public static Block PUMP = queueBlock(new PumpBlock("pump", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block TANK = queueBlock(new TankBlock("basic_tank", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block GLASS_TANK = queueBlock(new GlassTankBlock("basic_glass_tank", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block ITEM_BUFFER = queueBlock(new ItemBufferBlock("item_buffer", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.METAL)));
+    public static Block FLUID_METER = queueBlock(new FluidMeter("fluid_meter", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
+    public static Block FLUID_PORT = queueBlock(new FluidPortBlock("fluid_port", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block FLUID_DRAIN = queueBlock(new FluidDrainBlock("fluid_drain", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
+    public static Block SPIGOT = queueBlock(new SpigotBlock("spigot", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
 
-    public static Block registerBlock(String id, Block block)
-    {
-        return Registry.register(Registry.BLOCK, new Identifier(NeepMeat.NAMESPACE, id), block);
-    }
+    // --- Item Transfer ---
+    public static Block ITEM_DUCT = queueBlock(new ItemDuctBlock("item_duct", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.METAL)));
 
-    public static Block registerBlock(NMBlock block)
+    // --- Assembly ---
+    public static Block LINEAR_RAIL = queueBlock(new LinearRailBlock("linear_rail", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
+
+    public static Block BIG_LEVER = queueBlock(new BigLeverBlock("big_lever", FabricBlockSettings.of(Material.METAL).strength(4.0f)));
+
+    // --- Integrator ---
+    public static Block INTEGRATOR_EGG = queueBlock(new IntegratorEggBlock("integrator_egg", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.SLIME)));
+    public static Block TANK_WALL = queueBlock(new TankWallBlock("clear_tank_wall", 64, false, AbstractBlock.Settings.of(Material.GLASS).strength(0.3f).sounds(BlockSoundGroup.GLASS).nonOpaque().allowsSpawning(TankWallBlock::never).solidBlock(TankWallBlock::never).suffocates(TankWallBlock::never).blockVision(TankWallBlock::never)));
+
+    public static Block queueBlock(NMBlock block)
     {
         if (!(block instanceof Block))
         {
             throw new IllegalArgumentException("tried to register something that wasn't a block.");
         }
-        return Registry.register(Registry.BLOCK, new Identifier(NeepMeat.NAMESPACE, block.getRegistryName()), (Block) block);
+
+        BLOCKS.put(new Identifier(NeepMeat.NAMESPACE, block.getRegistryName()), block);
+        return (Block) block;
     }
 
     public static void registerBlocks()
     {
-
-        // --- Building Blocks ---
-        RUSTY_VENT = registerBlock(new BaseColumnBlock("rusty_vent", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE).nonOpaque()));
-        MESH_PANE = registerBlock(new BasePaneBlock("mesh_panel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
-        RUSTED_BARS = registerBlock(new BasePaneBlock("rusted_bars", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
-        RUSTY_PANEL = registerBlock(new BaseBlock("rusty_panel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        RUSTY_GRATE = registerBlock(new BaseBlock("rusty_grate", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-//        GREY_SMOOTH_TILE = registerBlock(new BaseBlock("smooth_tile_grey", 64, false, FabricBlockSettings.of(Material.STONE).strength(4.0f).sounds(BlockSoundGroup.STONE)));
-
-        SLOPE_TEST = registerBlock(new BaseStairsBlock(CAUTION_BLOCK.getDefaultState(), "slope_test", 64, FabricBlockSettings.of(Material.METAL).nonOpaque()));
-
-        SCAFFOLD_TRAPDOOR = registerBlock(new ScaffoldTrapdoorBlock("rusted_metal_scaffold_trapdoor", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE).nonOpaque()));
-
-        // --- General Blocks ---
-        PIPE = registerBlock(new FluidPipeBlock("pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        IRON_PIPE = registerBlock(new FluidPipeBlock("iron_pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        COPPER_PIPE = registerBlock(new FluidPipeBlock("copper_pipe", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        TROMMEL = registerBlock(new TrommelBlock("trommel", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
-        TROMMEL_CENTRE = registerBlock(new BaseDummyBlock("trommel_centre", FabricBlockSettings.of(Material.METAL).strength(4.0f)));
-        HEATER = registerBlock(new HeaterBlock("heater", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
-
-        // --- Fluid Transfer ---
-        PUMP = registerBlock(new PumpBlock("pump", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        TANK = registerBlock(new TankBlock("basic_tank", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        GLASS_TANK = registerBlock(new GlassTankBlock("basic_glass_tank", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        ITEM_BUFFER = registerBlock(new ItemBufferBlock("item_buffer", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.METAL)));
-        FLUID_METER = registerBlock(new FluidMeter("fluid_meter", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.LANTERN)));
-        FLUID_PORT = registerBlock(new FluidPortBlock("fluid_port", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        FLUID_DRAIN = registerBlock(new FluidDrainBlock("fluid_drain", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-        SPIGOT = registerBlock(new SpigotBlock("spigot", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.NETHERITE)));
-
-        // --- Item Transfer ---
-        ITEM_DUCT = registerBlock(new ItemDuctBlock("item_duct", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.METAL)));
-
-        // --- Assembly ---
-        LINEAR_RAIL = registerBlock(new LinearRailBlock("linear_rail", 64, false, FabricBlockSettings.of(Material.METAL).strength(4.0f)));
-
-        BIG_LEVER = registerBlock(new BigLeverBlock("big_lever", FabricBlockSettings.of(Material.METAL).strength(4.0f)));
-
-        // --- Integrator ---
-        INTEGRATOR_EGG = registerBlock(new IntegratorEggBlock("integrator_egg", 64, true, FabricBlockSettings.of(Material.METAL).strength(4.0f).sounds(BlockSoundGroup.SLIME)));
-        TANK_WALL = registerBlock(new TankWallBlock("clear_tank_wall", 64, false, AbstractBlock.Settings.of(Material.GLASS).strength(0.3f).sounds(BlockSoundGroup.GLASS).nonOpaque().allowsSpawning(TankWallBlock::never).solidBlock(TankWallBlock::never).suffocates(TankWallBlock::never).blockVision(TankWallBlock::never)));
-
-        for (NMBlock block : BLOCKS.values())
+        for (Map.Entry<Identifier, NMBlock> entry : BLOCKS.entrySet())
         {
-            registerBlock(block.getRegistryName(), (Block) block);
+//            registerBlock(entry.getKey(), (Block) entry.getValue());
+            Registry.register(Registry.BLOCK, entry.getKey(), (Block) entry.getValue());
         }
-
     }
-
-
 }
