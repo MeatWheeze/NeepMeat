6c035ca68f730aa495013db2b868417000201c40
neepmeat <o.o@oo.o>
2022-05-27 11:14:12 +0100

    Added rendering for ConverterBlock

--- src/main/java/com/neep/neepmeat/blockentity/ConverterBlockEntity.java
+++ src/main/java/com/neep/neepmeat/blockentity/ConverterBlockEntity.java
@@ -1,9 +1,8 @@
 package com.neep.neepmeat.blockentity;
 
-import com.neep.meatlib.block.BaseFacingBlock;
-import com.neep.neepmeat.block.machine.ConverterBlock;
 import com.neep.neepmeat.init.NMBlockEntities;
 import com.neep.neepmeat.init.NMFluids;
+import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
@@ -17,23 +16,28 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import org.lwjgl.system.CallbackI;
 
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class LargeConverterBlockEntity extends BlockEntity
+public class ConverterBlockEntity extends BlockEntity implements BlockEntityClientSerializable
 {
 
-    protected boolean isAssembled;
     protected int cooldown;
 
-    public LargeConverterBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state)
+    public boolean stage;
+    public boolean running;
+
+    // Rendering only
+    public float renderIn;
+    public float renderOut;
+
+    public ConverterBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state)
     {
         super(type, pos, state);
     }
 
-    public LargeConverterBlockEntity(BlockPos pos, BlockState state)
+    public ConverterBlockEntity(BlockPos pos, BlockState state)
     {
         this(NMBlockEntities.CONVERTER, pos, state);
     }
@@ -59,7 +63,9 @@ public class LargeConverterBlockEntity extends BlockEntity
         cooldown = 2;
 
         Transaction transaction = Transaction.openOuter();
-        convert(FluidConstants.BUCKET / 64, transaction);
+        long convertAmount = FluidConstants.BUCKET / 64;
+        this.running = convert(convertAmount, transaction) == convertAmount;
+        this.sync();
         transaction.commit();
     }
 
@@ -69,15 +75,20 @@ public class LargeConverterBlockEntity extends BlockEntity
         super.readNbt(nbt);
 
         this.cooldown = nbt.getInt("cooldown");
+//        this.stage = nbt.getShort("stage");
+//        this.inAmount = nbt.getLong("in_amount");
+//        this.outAmount = nbt.getLong("out_amount");
     }
 
     @Override
     public NbtCompound writeNbt(NbtCompound nbt)
     {
-        System.out.println(getCachedState());
         super.writeNbt(nbt);
 
         nbt.putInt("cooldown", cooldown);
+//        nbt.putShort("stage", stage);
+//        nbt.putLong("in_amount", inAmount);
+//        nbt.putLong("out_amount", outAmount);
 
         return nbt;
     }
@@ -99,8 +110,21 @@ public class LargeConverterBlockEntity extends BlockEntity
         return 0;
     }
 
-    public static <E extends BlockEntity> void serverTick(World world, BlockPos pos, BlockState state, LargeConverterBlockEntity be)
+    public static <E extends BlockEntity> void serverTick(World world, BlockPos pos, BlockState state, ConverterBlockEntity be)
     {
         be.tick();
+    }
+
+    @Override
+    public void fromClientTag(NbtCompound tag)
+    {
+        this.running = tag.getBoolean("running");
+    }
+
+    @Override
+    public NbtCompound toClientTag(NbtCompound tag)
+    {
+        tag.putBoolean("running", running);
+        return tag;
     }
 }
