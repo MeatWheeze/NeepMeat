d0096cf51cf9f3c2a63cdaf6bc065883fa0d8aee
neepmeat <o.o@oo.o>
2022-07-11 17:26:44 +0100

    Added datagen for wall tags

--- src/main/java/com/neep/meatlib/block/BaseWallBlock.java
+++ src/main/java/com/neep/meatlib/block/BaseWallBlock.java
@@ -1,8 +1,11 @@
 package com.neep.meatlib.block;
 
 import com.neep.meatlib.item.BaseBlockItem;
+import net.minecraft.block.Block;
 import net.minecraft.block.WallBlock;
 import net.minecraft.item.BlockItem;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.TagKey;
 
 public class BaseWallBlock extends WallBlock implements IMeatBlock
 {
@@ -16,6 +19,11 @@ public class BaseWallBlock extends WallBlock implements IMeatBlock
         this.registryName = blockName;
         this.maxStack = itemMaxStack;
         this.blockItem = new BaseBlockItem(this, blockName, itemMaxStack, false);
+    }
+
+    public TagKey<Block> getWallTag()
+    {
+        return BlockTags.WALLS;
     }
 
     @Override
