655220b249e1118dc99427e331878f810c7bf0c8
neepmeat <o.o@oo.o>
2022-07-12 15:59:59 +0100

    Fiddled

--- src/main/java/com/neep/neepmeat/block/pipe/IDataCable.java
+++ src/main/java/com/neep/neepmeat/block/pipe/IDataCable.java
@@ -1,5 +1,14 @@
 package com.neep.neepmeat.block.pipe;
 
-public class IDataCable
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+
+public interface IDataCable
 {
+    default boolean connectInDirection(BlockView world, BlockPos pos, BlockState state, Direction direction)
+    {
+        return true;
+    }
 }
