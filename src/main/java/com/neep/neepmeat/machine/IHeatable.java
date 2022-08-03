814d9a46ee4374ae48b6422c6548aad6d6c4e629
neepmeat <o.o@oo.o>
2022-08-03 19:13:59 +0100

    feat: Make furnace speed change according to connected heater
    - Added interface injection of IHeatable to AbstractFurnaceBlockEntity.
    - Added a mixin inject into the start of
      AbstractFurnaceBlockEntity.tick() that increases cookTime according to
      the connected heater's heat.

--- src/main/java/com/neep/neepmeat/machine/IHeatable.java
+++ src/main/java/com/neep/neepmeat/machine/IHeatable.java
@@ -1,28 +1,26 @@
 package com.neep.neepmeat.machine;
 
-import com.neep.neepmeat.mixin.FurnaceAccessor;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public interface IHeatable
 {
-    default void setBurning()
-    {
-        if (this instanceof FurnaceAccessor accessor)
-        {
-            accessor.setBurnTime(2);
-        }
-    }
+    void setBurning();
+
+    int getFuelTime();
+
+    int getCurrentBurnTime();
+
+    int getCookTimeTotal();
+
+    int getCookTime();
+
+    void setCookTime(int time);
 
     void updateState(World world, BlockPos pos, BlockState oldState);
 
-    default int getCurrentBurnTime()
-    {
-        if (this instanceof FurnaceAccessor accessor)
-        {
-            return accessor.getBurnTime();
-        }
-        return 0;
-    }
+    void setHeatMultiplier(float multiplier);
+    float getHeatMultiplier();
+
 }
