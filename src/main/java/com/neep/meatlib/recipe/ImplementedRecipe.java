6076fd79949a3b46f6d203219a3ef0590259027b
neepmeat <o.o@oo.o>
2022-08-08 11:42:58 +0100

    feat: Add ImplementedRecipe to simplify the creation of new recipe types

---
+++ src/main/java/com/neep/meatlib/recipe/ImplementedRecipe.java
@@ -0,0 +1,39 @@
+package com.neep.meatlib.recipe;
+
+import com.neep.neepmeat.inventory.ImplementedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.util.collection.DefaultedList;
+
+/**
+ * This class overrides some Vanilla recipe methods so that they do not have to be overridden in every sub-class.
+ */
+public abstract class ImplementedRecipe<T extends ImplementedRecipe.DummyInventory> implements Recipe<T>
+{
+    @Override
+    public ItemStack craft(T inventory)
+    {
+        throw new UnsupportedOperationException("Vanilla crafting methods are not supported");
+    }
+
+    @Override
+    public boolean fits(int width, int height)
+    {
+        return false;
+    }
+
+    @Override
+    public ItemStack getOutput()
+    {
+        throw new UnsupportedOperationException("Vanilla crafting methods are not supported");
+    }
+
+    public interface DummyInventory extends ImplementedInventory
+    {
+        @Override
+        default DefaultedList<ItemStack> getItems()
+        {
+            throw new UnsupportedOperationException("This object is not a real inventory");
+        }
+    }
+}
