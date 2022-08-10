937a50db1b734693e1bfb86c2782d0541cf82ec7
neepmeat <o.o@oo.o>
2022-08-10 23:34:26 +0100

    Continue working on Trommel

--- src/main/java/com/neep/neepmeat/machine/trommel/TrommelStorage.java
+++ src/main/java/com/neep/neepmeat/machine/trommel/TrommelStorage.java
@@ -1,10 +1,16 @@
 package com.neep.neepmeat.machine.trommel;
 
 import com.neep.meatlib.util.NbtSerialisable;
+import com.neep.neepmeat.api.storage.WritableSingleFluidStorage;
+import com.neep.neepmeat.storage.WritableStackStorage;
 import net.minecraft.nbt.NbtCompound;
 
 public class TrommelStorage implements NbtSerialisable
 {
+    protected WritableSingleFluidStorage fluidInput;
+    protected WritableSingleFluidStorage fluidOutput;
+    protected WritableStackStorage itemOutput;
+
     public TrommelStorage(TrommelBlockEntity parent)
     {
 
