25bd5ce77cbe6d1a384f0a42f228d0cdc474bed6
neepmeat <o.o@oo.o>
2022-08-16 19:54:28 +0100

    feat: Add recipes for ore processing machines

--- src/main/java/com/neep/neepmeat/init/NMLootTables.java
+++ src/main/java/com/neep/neepmeat/init/NMLootTables.java
@@ -1,30 +1,21 @@
 package com.neep.neepmeat.init;
 
 import com.neep.meatlib.block.BaseCropBlock;
-import com.neep.meatlib.registry.BlockRegistry;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.data.server.BlockLootTableGenerator;
 import net.minecraft.entity.EntityType;
-import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.KilledByPlayerLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LeafEntry;
 import net.minecraft.loot.entry.LootPoolEntry;
-import net.minecraft.loot.function.EnchantRandomlyLootFunction;
 import net.minecraft.loot.function.LootingEnchantLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
-
-import java.util.Map;
 
 public class NMLootTables
 {
@@ -32,7 +23,7 @@ public class NMLootTables
     private static final Identifier DUNGEON = LootTables.SIMPLE_DUNGEON_CHEST;
     private static final Identifier GRASS = Blocks.GRASS.getLootTableId();
     private static final Identifier TALL_GRASS = Blocks.TALL_GRASS.getLootTableId();
-    private static final Identifier POTATO = Blocks.POTATOES.getLootTableId();
+    private static final Identifier POTATOES = Blocks.POTATOES.getLootTableId();
 
     public static void init()
     {
@@ -80,7 +71,7 @@ public class NMLootTables
 
                 tableBuilder.pool(builder);
             }
-            if (POTATO.equals(id))
+            if (POTATOES.equals(id))
             {
                 LootPool.Builder builder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
