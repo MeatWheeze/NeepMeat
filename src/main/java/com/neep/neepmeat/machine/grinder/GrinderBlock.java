905d7ee3f03502df969864b6babf87f0e3424c8d
neepmeat <o.o@oo.o>
2022-08-28 15:31:19 +0100

    feat(Crusher): Support inserting dropped item entities
    - Added hand-interaction with the input storages

--- src/main/java/com/neep/neepmeat/machine/grinder/GrinderBlock.java
+++ src/main/java/com/neep/neepmeat/machine/grinder/GrinderBlock.java
@@ -3,16 +3,24 @@ package com.neep.neepmeat.machine.grinder;
 import com.neep.meatlib.block.BaseHorFacingBlock;
 import com.neep.neepmeat.machine.content_detector.ContentDetectorBlock;
 import com.neep.neepmeat.recipe.GrindingRecipe;
+import com.neep.neepmeat.util.ItemUtils;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -30,6 +38,16 @@ public class GrinderBlock extends BaseHorFacingBlock implements BlockEntityProvi
     public BlockState getPlacementState(ItemPlacementContext context)
     {
         return this.getDefaultState().with(FACING, context.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit)
+    {
+        if (world.getBlockEntity(pos) instanceof GrinderBlockEntity be)
+        {
+            return ActionResult.success(ItemUtils.singleVariantInteract(player, hand, be.getStorage().getInputStorage()));
+        }
+        return ActionResult.SUCCESS;
     }
 
     @Nullable
@@ -85,15 +103,34 @@ public class GrinderBlock extends BaseHorFacingBlock implements BlockEntityProvi
             ItemStack stack1 = be.getStorage().getInputStorage().getResource().toStack(1);
             ItemStack stack2 = recipe.getItemOutput().resource().getDefaultStack();
 
-            double d = (double) pos.getX() + 0.5;
-            double e = pos.getY() + 0.7;
-            double f = (double) pos.getZ() + 0.5;
+            for (int it = 0; it < 5; ++it)
+            {
+                double d = (double) pos.getX() + 0.5;
+                double e = pos.getY() + 0.7;
+                double f = (double) pos.getZ() + 0.5;
 
-            double h = random.nextDouble() * 0.1;
-            double i = random.nextDouble() * 0.1 + 0.2;
-            double j = random.nextDouble() * 0.1;
+                double h = (random.nextDouble() - 0.5) * 0.1;
+                double i = (random.nextDouble() - 0.5) * 0.1 + 0.2;
+                double j = (random.nextDouble() - 0.5) * 0.1;
 
-            world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack1.isEmpty() ? stack2 : stack1), d, e, f, h, i, j);
+                world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, stack1.isEmpty() ? stack2 : stack1), d, e, f, h, i, j);
+            }
+        }
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance)
+    {
+        super.onLandedUpon(world, state, pos, entity, fallDistance);
+        if (world.getBlockEntity(pos) instanceof GrinderBlockEntity be && !world.isClient() && entity instanceof ItemEntity item)
+        {
+            try (Transaction transaction = Transaction.openOuter())
+            {
+                ItemVariant variant = ItemVariant.of(item.getStack());
+                long inserted = be.getStorage().getInputStorage().insert(variant, item.getStack().getCount(), transaction);
+                item.getStack().decrement((int) inserted);
+                transaction.commit();
+            }
         }
     }
 }
