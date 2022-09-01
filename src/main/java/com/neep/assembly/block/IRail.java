140e173b2f4b804a429ca9967a19f4a78e192f3f
neepmeat <o.o@oo.o>
2022-09-01 18:05:09 +0100

    refactor: Remove Assembly entirely

--- src/main/java/com/neep/assembly/block/IRail.java
+++
@@ -1,9 +0,0 @@
-package com.neep.assembly.block;
-
-import com.neep.neepmeat.util.LinearDirection;
-import net.minecraft.state.property.EnumProperty;
-
-public interface IRail
-{
-    EnumProperty<LinearDirection> DIRECTION = EnumProperty.of("direction", LinearDirection.class);
-}
