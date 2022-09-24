59d80953a684ac6eec41c34ec306b4f3c0023b00
neepmeat <o.o@oo.o>
2022-09-24 22:59:07 +0100

    refactor: Unify mixin names

--- src/main/java/com/neep/neepmeat/mixin/MixinParticleManager.java
+++ src/main/java/com/neep/neepmeat/mixin/MixinParticleManager.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ParticleManager.SimpleSpriteProvider.class)
-public interface ParticleManagerMixin
+public interface MixinParticleManager
 {
     @Invoker("<init>")
     static ParticleManager.SimpleSpriteProvider invokeConstructor()
