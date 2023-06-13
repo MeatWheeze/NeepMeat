5d38d7056c4f70fa5cd10e079caad742560f456a
MeatWheeze <ooo@oo.com>
2023-06-13 22:26:02 +0100

    neepmeat: Fix saving player upgrades

--- src/main/java/com/neep/neepmeat/network/PlayerUpgradeStatusS2CPacket.java
+++ src/main/java/com/neep/neepmeat/network/PlayerUpgradeStatusS2CPacket.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
@@ -14,6 +15,16 @@ import net.minecraft.util.Identifier;
 public class PlayerUpgradeStatusS2CPacket
 {
     public static final Identifier ID = new Identifier(NeepMeat.NAMESPACE, "player_upgrade_install");
+
+    public static void sendLoad(ServerPlayerEntity player, NbtCompound initialNbt)
+    {
+        PacketByteBuf buf = PacketByteBufs.create();
+
+        buf.writeVarInt(Status.LOAD.ordinal());
+        buf.writeNbt(initialNbt);
+
+        ServerPlayNetworking.send(player, ID, buf);
+    }
 
     public static void send(ServerPlayerEntity player, Identifier upgradeId, Status status)
     {
@@ -39,6 +50,11 @@ public class PlayerUpgradeStatusS2CPacket
                 {
                     case INSTALL -> PlayerUpgradeManager.get(client.player).installUpgrade(buf.readIdentifier());
                     case REMOVE -> PlayerUpgradeManager.get(client.player).removeUpgrade(buf.readIdentifier());
+                    case LOAD ->
+                    {
+                        NbtCompound fullNbt = buf.readNbt();
+                        PlayerUpgradeManager.get(client.player).sync(fullNbt);
+                    }
                 }
             });
         }
@@ -47,6 +63,7 @@ public class PlayerUpgradeStatusS2CPacket
     public enum Status
     {
         INSTALL,
-        REMOVE;
+        REMOVE,
+        LOAD;
     }
 }
--- src/main/java/com/neep/neepmeat/player/upgrade/PlayerUpgradeManager.java
+++ src/main/java/com/neep/neepmeat/player/upgrade/PlayerUpgradeManager.java
@@ -1,16 +1,21 @@
 package com.neep.neepmeat.player.upgrade;
 
 import com.google.common.collect.Maps;
+import com.neep.meatlib.api.event.EntityNbtEvents;
+import com.neep.meatlib.api.event.InitialTicks;
 import com.neep.meatlib.attachment.player.PlayerAttachment;
 import com.neep.meatlib.util.NbtSerialisable;
 import com.neep.neepmeat.network.PlayerUpgradeStatusS2CPacket;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.util.NbtType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 
 import java.util.Map;
@@ -21,6 +26,8 @@ public class PlayerUpgradeManager implements PlayerAttachment, NbtSerialisable
     protected PlayerEntity player;
     protected Map<Identifier, PlayerUpgrade> upgrades = Maps.newHashMap();
 
+    protected static final String KEY_ROOT = "neepmeat:upgrades";
+
     public PlayerUpgradeManager(PlayerEntity player)
     {
         this.player = player;
@@ -29,22 +36,47 @@ public class PlayerUpgradeManager implements PlayerAttachment, NbtSerialisable
     public void installUpgrade(Identifier id)
     {
 
-        PlayerUpgradeRegistry.PlayerUpgradeConstructor constructor = PlayerUpgradeRegistry.REGISTRY.get(id);
-        if (constructor != null)
+        if (PlayerUpgradeRegistry.REGISTRY.containsId(id))
         {
             if (!player.getWorld().isClient())
             {
                 PlayerUpgradeStatusS2CPacket.send((ServerPlayerEntity) player, id, PlayerUpgradeStatusS2CPacket.Status.INSTALL);
             }
 
-            PlayerUpgrade upgrade = constructor.create(player);
-            upgrades.put(id, constructor.create(player));
-            upgrade.onInstall();
+            addUpgrade(id);
         }
         else
         {
             throw new IllegalArgumentException("Tried to add an unregistered player upgrade");
         }
+    }
+
+    protected void addUpgrade(Identifier id)
+    {
+        PlayerUpgradeRegistry.PlayerUpgradeConstructor constructor = PlayerUpgradeRegistry.REGISTRY.get(id);;
+
+        if (constructor == null) throw new IllegalArgumentException("Tried to add an unregistered player upgrade");
+
+        PlayerUpgrade upgrade = constructor.create(player);
+        upgrades.put(id, constructor.create(player));
+        upgrade.onInstall();
+    }
+
+    public void sync(NbtCompound fullNbt)
+    {
+        readNbt(fullNbt);
+//        PlayerUpgradeRegistry.PlayerUpgradeConstructor constructor = PlayerUpgradeRegistry.REGISTRY.get(id);
+//        if (constructor != null)
+//        {
+//            PlayerUpgrade upgrade = constructor.create(player);
+//            upgrades.put(id, constructor.create(player));
+//            upgrade.readNbt(initialNbt);
+//            upgrade.onInstall();
+//        }
+//        else
+//        {
+//            throw new IllegalArgumentException("Tried to add an unregistered player upgrade");
+//        }
     }
 
     public void removeUpgrade(Identifier id)
@@ -78,16 +110,44 @@ public class PlayerUpgradeManager implements PlayerAttachment, NbtSerialisable
         return (float) upgrades.values().stream().mapToDouble(u -> u.getProtectionAmount(source, amount)).sum();
     }
 
+    protected void addAll(PlayerUpgradeManager manager)
+    {
+        upgrades.putAll(manager.upgrades);
+    }
+
+    protected void deferredLoad(NbtCompound fullNbt)
+    {
+        if (fullNbt != null && player instanceof ServerPlayerEntity serverPlayer)
+        {
+            PlayerUpgradeStatusS2CPacket.sendLoad(serverPlayer, fullNbt);
+        }
+    }
+
     @Override
     public NbtCompound writeNbt(NbtCompound nbt)
     {
+        NbtList list = new NbtList();
+        upgrades.forEach((i, u) ->
+        {
+            NbtCompound nbt1 = new NbtCompound();
+            nbt1.putString("id", i.toString());
+            u.writeNbt(nbt1);
+            list.add(nbt1);
+        });
+        nbt.put("upgrades", list);
         return nbt;
     }
 
     @Override
     public void readNbt(NbtCompound nbt)
     {
-
+        NbtList list = nbt.getList("upgrades", NbtType.COMPOUND);
+        list.forEach(nbt1 ->
+        {
+            NbtCompound upgradeNbt = (NbtCompound) nbt1;
+            Identifier id = Identifier.tryParse(upgradeNbt.getString("id"));
+            addUpgrade(id);
+        });
     }
 
     public static PlayerUpgradeManager get(PlayerEntity player)
@@ -110,12 +170,18 @@ public class PlayerUpgradeManager implements PlayerAttachment, NbtSerialisable
 
         ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) ->
         {
-            get(newPlayer).upgrades.values().forEach(u -> u.onRespawn(oldPlayer, newPlayer));
+            PlayerUpgradeManager oldManager = get(oldPlayer);
+            PlayerUpgradeManager newManager = get(newPlayer);
+
+            // Sync old upgrades to new manager
+            newManager.addAll(oldManager);
+            newManager.upgrades.values().forEach(u -> u.onRespawn(oldPlayer, newPlayer));
         });
 
         ServerPlayConnectionEvents.DISCONNECT.register((handler, server) ->
         {
-            get(handler.getPlayer()).upgrades.values().forEach(PlayerUpgrade::onPlayerRemove);
+            PlayerUpgradeManager manager = get(handler.getPlayer());
+            manager.upgrades.values().forEach(PlayerUpgrade::onPlayerRemove);
         });
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) ->
@@ -123,6 +189,39 @@ public class PlayerUpgradeManager implements PlayerAttachment, NbtSerialisable
             if (client.player != null)
                 get(client.player).upgrades.values().forEach(PlayerUpgrade::onPlayerRemove);
         });
-    }
 
+        ServerPlayConnectionEvents.JOIN.register(((handler, sender, server) ->
+        {
+            // Sync newly loaded data to the client player.
+//            PlayerUpgradeStatusS2CPacket.sendLoad(handler.getPlayer(), );
+        }));
+
+        EntityNbtEvents.WRITE.register((entity, nbt) ->
+        {
+            if (entity instanceof PlayerEntity player1)
+            {
+                NbtCompound nbtCompound = new NbtCompound();
+                get(player1).writeNbt(nbtCompound);
+
+                // Insert upgrade manager NBT
+                nbt.put(KEY_ROOT, nbtCompound);
+            }
+        });
+
+        EntityNbtEvents.READ.register((entity, nbt) ->
+        {
+            if (entity instanceof PlayerEntity player1 && player1.getWorld() instanceof ServerWorld serverWorld)
+            {
+                NbtCompound nbtCompound = nbt.getCompound(KEY_ROOT);
+
+                // Unsure whether this state is possible
+                if (nbtCompound == null) return;
+
+                PlayerUpgradeManager manager = get(player1);
+                manager.readNbt(nbtCompound);
+
+                InitialTicks.getInstance(serverWorld).queue(w -> manager.deferredLoad(nbtCompound));
+            }
+        });
+    }
 }
