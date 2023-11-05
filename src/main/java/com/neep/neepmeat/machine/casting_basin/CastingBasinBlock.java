8f8bf8da12eb16febab4148a171fbd6b3f1af8eb
MeatWheeze <ooo@oo.com>
2023-11-05 18:58:39 +0000

    Changed some textures for Pressing Basin and Fat Collector

--- src/main/java/com/neep/neepmeat/machine/casting_basin/CastingBasinBlock.java
+++ src/main/java/com/neep/neepmeat/machine/casting_basin/CastingBasinBlock.java
@@ -4,22 +4,34 @@ import com.neep.meatlib.block.BaseBlock;
 import com.neep.meatlib.item.ItemSettings;
 import com.neep.neepmeat.init.NMBlockEntities;
 import com.neep.neepmeat.util.ItemUtils;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class CastingBasinBlock extends BaseBlock implements BlockEntityProvider
 {
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 14, 16);
+
     public CastingBasinBlock(String registryName, ItemSettings itemSettings, Settings settings)
     {
         super(registryName, itemSettings, settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
+    {
+        return SHAPE;
     }
 
     @Override
