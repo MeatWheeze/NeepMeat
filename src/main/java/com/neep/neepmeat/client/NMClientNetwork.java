5275090d7c036b0b0d0135863674b4c32e9eaa86
MeatWheeze <ooo@oo.com>
2023-11-26 22:14:17 +0000

    Improved communication between client and PLC

--- src/main/java/com/neep/neepmeat/client/NMClientNetwork.java
+++ src/main/java/com/neep/neepmeat/client/NMClientNetwork.java
@@ -1,11 +1,13 @@
 package com.neep.neepmeat.client;
 
 import com.neep.neepmeat.network.plc.PLCRobotEnterS2C;
+import com.neep.neepmeat.network.plc.PLCSyncProgram;
 
 public class NMClientNetwork
 {
     public static void init()
     {
         PLCRobotEnterS2C.Client.registerReceiver();
+        PLCSyncProgram.Client.registerReceiver();
     }
 }
