79df0f34919d85cc9a6cde054c92e8b7cb6e807a
MeatWheeze <ooo@oo.com>
2023-12-09 14:33:19 +0000

    Textured robotic arm

--- src/main/java/com/neep/neepmeat/client/renderer/RoboticArmRenderer.java
+++ src/main/java/com/neep/neepmeat/client/renderer/RoboticArmRenderer.java
@@ -7,7 +7,6 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.Vec3f;
 
 import static java.lang.Math.cos;
@@ -25,38 +24,51 @@ public class RoboticArmRenderer implements BlockEntityRenderer<RoboticArmBlockEn
     {
         float t = be.getWorld().getTime() + tickDelta;
 
+        var player = MinecraftClient.getInstance().player;
+
+        double targetX = player.getX();
+        double targetY = player.getY();
+        double targetZ = player.getZ();
+
+        double relX = targetX - (be.getPos().getX() + 0.5);
+        double relY = targetY - (be.getPos().getY() + 1 + 2 / 16f);
+        double relZ = targetZ - (be.getPos().getZ() + 0.5);
+
+        float yaw = (float) Math.toDegrees(Math.atan2(relX, relZ));
+
         float l = 2 + 5 / 16f;
-        double x = 1;
-        double y = (float) (1 + Math.sin(t / 10));
-//        double y = 1.5;
+        double lx = Math.sqrt(relX * relX + relZ * relZ);
+        double ly = relY;
 
-        double d5 = Math.sqrt(x * x + y * y);
+        double d5 = Math.sqrt(lx * lx + ly * ly);
 
-        double x1 = x / d5 * Math.min(d5, 2 * l - 0.0001);
-        double y1 = y / d5 * Math.min(d5, 2 * l - 0.0001);
+        // Constrain
+        double x1 = lx / d5 * Math.min(d5, 2 * l - 0.0001);
+        double y1 = ly / d5 * Math.min(d5, 2 * l - 0.0001);
 
         double d = Math.sqrt((x1 * x1 + y1 * y1));
 
-        double x11 = x1 / d;
-        double y11 = y1 / d;
+        double x2 = x1 / d;
+        double y2 = y1 / d;
 
+        // ???
         double a = Math.acos((-(d * d)) / (-2 * d * l));
 
-        double d3 = l * (x11 * cos(a) - y11 * sin(a));
-        double d4 = l * (x11 * sin(a) + y11 * cos(a));
+        // Rotation matrix
+        double x3 = l * (x2 * cos(a) - y2 * sin(a));
+        double y3 = l * (x2 * sin(a) + y2 * cos(a));
 
-        if (be.getWorld().getTime() % 4 == 0)
-        {
-            MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.SMOKE,
-                    be.getPos().getX() + x + 0.5, (1 + 1 / 16f) + be.getPos().getY() + y, be.getPos().getZ() + 0.5,
-                    0, 0, 0);
+//        if (be.getWorld().getTime() % 4 == 0)
+//        {
+//            MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.SMOKE,
+//                    be.getPos().getX() + lx + 0.5, (1 + 1 / 16f) + be.getPos().getY() + ly, be.getPos().getZ() + 0.5,
+//                    0, 0, 0);
+//
+//            MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.SMOKE,
+//                    be.getPos().getX() + x3 + 0.5, (1 + 1 / 16f) + be.getPos().getY() + y3, be.getPos().getZ() + 0.5,
+//                    0, 0, 0);
+//        }
 
-            MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.SMOKE,
-                    be.getPos().getX() + d3 + 0.5, (1 + 1 / 16f) + be.getPos().getY() + d4, be.getPos().getZ() + 0.5,
-                    0, 0, 0);
-        }
-
-        float yaw = 90;
 
         matrices.push();
         matrices.translate(0.5, 0.5, 0.5);
@@ -68,19 +80,19 @@ public class RoboticArmRenderer implements BlockEntityRenderer<RoboticArmBlockEn
         matrices.push();
         matrices.translate(0, 2 - 1 / 16f, 0);
         matrices.translate(0.5, -12 / 16f, 0.5);
-        double angle1 = Math.asin(d3 / Math.sqrt(d3 * d3 + d4 * d4));
+        double angle1 = Math.atan2(x3, y3);
         matrices.multiply(Vec3f.POSITIVE_X.getRadialQuaternion((float) angle1));
         matrices.translate(-0.5, 12 / 16f, -0.5);
-        BERenderUtils.renderModel(NMExtraModels.ROBOTIC_ARM_SEGMENT_1, matrices, be.getWorld(), be.getPos().up(), be.getCachedState(), vertexConsumers);
+        BERenderUtils.renderModelSmooth(NMExtraModels.ROBOTIC_ARM_SEGMENT_1, matrices, be.getWorld(), be.getPos().up(), be.getCachedState(), vertexConsumers, be.getWorld().getRandom());
         matrices.pop();
 
 //        matrices.translate(0, 34 / 16f, 0);
-        matrices.translate(0, d4 + 2 - 1 / 16f, d3);
+        matrices.translate(0, y3 + 2 - 1 / 16f, x3);
         matrices.translate(0.5, -12 / 16f, 0.5);
-        double angle2 = Math.atan2((x - d3), (y - d4));
+        double angle2 = Math.atan2((lx - x3), (ly - y3));
         matrices.multiply(Vec3f.POSITIVE_X.getRadialQuaternion((float) angle2));
         matrices.translate(-0.5, 12 / 16f, -0.5);
-        BERenderUtils.renderModel(NMExtraModels.ROBOTIC_ARM_SEGMENT_2, matrices, be.getWorld(), be.getPos().up(), be.getCachedState(), vertexConsumers);
+        BERenderUtils.renderModelSmooth(NMExtraModels.ROBOTIC_ARM_SEGMENT_2, matrices, be.getWorld(), be.getPos().up(), be.getCachedState(), vertexConsumers, be.getWorld().getRandom());
         matrices.pop();
 
     }
