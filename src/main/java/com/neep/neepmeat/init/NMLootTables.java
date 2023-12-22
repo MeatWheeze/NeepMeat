edcc18000f7c445bb913e2bc520f5ae40332c66c
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:29 +0000

    Added Integrator egg to more locations; ran datagen

--- src/main/java/com/neep/neepmeat/init/NMLootTables.java
+++ src/main/java/com/neep/neepmeat/init/NMLootTables.java
@@ -22,6 +22,10 @@ public class NMLootTables
     private static final Identifier ZOMBIE = EntityType.ZOMBIE.getLootTableId();
     private static final Identifier ELDER_GUARDIAN = EntityType.ELDER_GUARDIAN.getLootTableId();
     private static final Identifier DUNGEON = LootTables.SIMPLE_DUNGEON_CHEST;
+    private static final Identifier DESERT_TEMPLE = LootTables.DESERT_PYRAMID_CHEST;
+    private static final Identifier JUNGLE_TEMPLE = LootTables.JUNGLE_TEMPLE_CHEST;
+    private static final Identifier ARMORER_CHEST = LootTables.VILLAGE_ARMORER_CHEST;
+    private static final Identifier BUTCHER_CHEST = LootTables.VILLAGE_BUTCHER_CHEST;
     private static final Identifier GRASS = Blocks.GRASS.getLootTableId();
     private static final Identifier TALL_GRASS = Blocks.TALL_GRASS.getLootTableId();
     private static final Identifier POTATOES = Blocks.POTATOES.getLootTableId();
@@ -51,14 +55,14 @@ public class NMLootTables
             {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0f))
-                        .with((LootPoolEntry.Builder<?>)((LeafEntry.Builder)ItemEntry.builder(NMBlocks.STATUE.asItem())
+                        .with(((LeafEntry.Builder<?>)ItemEntry.builder(NMBlocks.STATUE.asItem())
                                 .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f))))
                                 .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f))))
                         .conditionally(KilledByPlayerLootCondition.builder());
 
                 tableBuilder.pool(poolBuilder);
             }
-            else if (DUNGEON.equals(id))
+            else if (DUNGEON.equals(id) || DESERT_TEMPLE.equals(id) || ARMORER_CHEST.equals(id) || BUTCHER_CHEST.equals(id) || JUNGLE_TEMPLE.equals(id))
             {
                 LootPool.Builder builder = LootPool.builder()
                     .rolls(UniformLootNumberProvider.create(0.0f, 1.0f))
