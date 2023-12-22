1d0a72fbb2caed97c1ca71b8d0cef22e7a12ada3
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:03 +0000

    Added Motor Block

---
+++ src/main/java/com/neep/neepmeat/block/machine/MotorBlock.java
@@ -0,0 +1,13 @@
+package com.neep.neepmeat.block.machine;
+
+import com.neep.meatlib.block.BaseFacingBlock;
+
+public class MotorBlock extends BaseFacingBlock
+{
+    public MotorBlock(String itemName, int itemMaxStack, boolean hasLore, Settings settings)
+    {
+        super(itemName, itemMaxStack, hasLore, settings);
+    }
+
+
+}
