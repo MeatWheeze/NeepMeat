4414a632a67c929a65a0d4632079229e7cb20d81
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:12 +0000

    feat(Death Blades): Convert entities into meat on death

--- src/main/java/com/neep/neepmeat/init/NMParticles.java
+++ src/main/java/com/neep/neepmeat/init/NMParticles.java
@@ -1,6 +1,7 @@
 package com.neep.neepmeat.init;
 
 import com.neep.meatlib.registry.ParticleRegistry;
+import com.neep.meatweapons.MeatWeapons;
 import com.neep.neepmeat.NeepMeat;
 import com.neep.neepmeat.mixin.ParticleManagerMixin;
 import com.neep.neepmeat.particle.SwirlingParticle;
@@ -9,24 +10,27 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.fabricmc.fabric.mixin.client.particle.ParticleManagerAccessor;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.particle.ParticleFactory;
-import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.particle.SpriteProvider;
+import net.minecraft.client.particle.*;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class NMParticles
 {
     public static final ParticleType<SwirlingParticleEffect> BLOCK_SWIRL = ParticleRegistry.register(NeepMeat.NAMESPACE, "block_swirl", SwirlingParticleEffect.PARAMETERS_FACTORY, SwirlingParticleEffect::createCodec);
-    public static DefaultParticleType PLASMA_PARTICLE = FabricParticleTypes.simple();
+
+    public static DefaultParticleType MEAT_SPLASH = FabricParticleTypes.simple();
+
     public static void init()
     {
-//            registerFactory(client.particleManager, BLOCK_SWIRL, SwirlingParticle.Factory::new);
+        MEAT_SPLASH = ParticleRegistry.register(NeepMeat.NAMESPACE, "meat_splash", MEAT_SPLASH);
     }
 
     @Environment(value = EnvType.CLIENT)
@@ -34,7 +38,15 @@ public class NMParticles
     {
         public static void init()
         {
+            ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) ->
+            {
+                registry.register(new Identifier(NeepMeat.NAMESPACE, "particle/meat_splash_0"));
+                registry.register(new Identifier(NeepMeat.NAMESPACE, "particle/meat_splash_1"));
+                registry.register(new Identifier(NeepMeat.NAMESPACE, "particle/meat_splash_2"));
+            }));
+
             ParticleFactoryRegistry.getInstance().register(BLOCK_SWIRL, new SwirlingParticle.Factory());
+            ParticleFactoryRegistry.getInstance().register(MEAT_SPLASH, FlameParticle.Factory::new);
         }
 
         private static <T extends ParticleEffect> void registerFactory(ParticleType<T> type, SpriteAwareFactory<?> factory)
--- src/main/java/com/neep/neepmeat/machine/death_blades/DeathBladesBlock.java
+++ src/main/java/com/neep/neepmeat/machine/death_blades/DeathBladesBlock.java
@@ -2,23 +2,40 @@ package com.neep.neepmeat.machine.death_blades;
 
 import com.neep.meatlib.block.BaseFacingBlock;
 import com.neep.neepmeat.init.NMBlockEntities;
-import com.neep.neepmeat.machine.converter.ConverterBlockEntity;
-import com.neep.neepmeat.util.MiscUtils;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityTicker;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public class DeathBladesBlock extends BaseFacingBlock implements BlockEntityProvider
 {
+    public static final Map<Direction, VoxelShape> SHAPES = Map.of(
+            Direction.NORTH, Block.createCuboidShape(4, 4, 4, 12, 12, 16),
+            Direction.EAST, Block.createCuboidShape(0, 4, 4, 12, 12, 12),
+            Direction.SOUTH, Block.createCuboidShape(4, 4, 0, 12, 12, 12),
+            Direction.WEST, Block.createCuboidShape(4, 4, 4, 16, 12, 12),
+            Direction.UP, Block.createCuboidShape(4, 0, 4, 12, 12, 12),
+            Direction.DOWN, Block.createCuboidShape(4, 4, 4, 12, 16, 12)
+    );
+
     public DeathBladesBlock(String itemName, int itemMaxStack, boolean hasLore, Settings settings)
     {
         super(itemName, itemMaxStack, hasLore, settings.nonOpaque());
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
+    {
+        return SHAPES.get(state.get(FACING));
     }
 
     @Override
@@ -33,10 +50,4 @@ public class DeathBladesBlock extends BaseFacingBlock implements BlockEntityProv
     {
         return NMBlockEntities.DEATH_BLADES.instantiate(pos, state);
     }
-
-//    @Override
-//    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type)
-//    {
-//        return MiscUtils.checkType(type, NMBlockEntities.DEATH_BLADES, DeathBladesBlockEntity::serverTick, world);
-//    }
 }
