f9cafbca89ba2c3689f9c8068e00ef462f612947
MeatWheeze <ooo@oo.com>
2023-12-22 18:45:56 +0000

    Heater consumes blood and heats furnaces.

--- src/main/java/com/neep/neepmeat/blockentity/machine/BloodMachineBlockEntity.java
+++ src/main/java/com/neep/neepmeat/blockentity/machine/BloodMachineBlockEntity.java
@@ -1,26 +1,34 @@
 package com.neep.neepmeat.blockentity.machine;
 
 import com.neep.neepmeat.fluid_util.FluidBuffer;
+import com.neep.neepmeat.fluid_util.storage.MultiTypedFluidBuffer;
 import com.neep.neepmeat.fluid_util.storage.TypedFluidBuffer;
-import com.neep.neepmeat.init.BlockEntityInitialiser;
 import com.neep.neepmeat.init.FluidInitialiser;
+import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
-public abstract class BloodMachineBlockEntity<T extends BloodMachineBlockEntity> extends BlockEntity implements FluidBuffer.FluidBufferProvider
+import java.util.List;
+
+public abstract class BloodMachineBlockEntity<T extends BloodMachineBlockEntity> extends BlockEntity implements FluidBuffer.FluidBufferProvider, BlockEntityClientSerializable
 {
-    protected TypedFluidBuffer buffer;
+    public TypedFluidBuffer inputBuffer;
+    public TypedFluidBuffer outputBuffer;
+    protected MultiTypedFluidBuffer buffer;
 
     public BloodMachineBlockEntity(BlockEntityType<T> type, BlockPos pos, BlockState state)
     {
         super(type, pos, state);
-        buffer = new TypedFluidBuffer(this, 4 * FluidConstants.BUCKET, fluidVariant -> fluidVariant.isOf(FluidInitialiser.STILL_ENRICHED_BLOOD));
+        inputBuffer = new TypedFluidBuffer(this, 4 * FluidConstants.BUCKET, fluidVariant -> fluidVariant.isOf(FluidInitialiser.STILL_ENRICHED_BLOOD), TypedFluidBuffer.Mode.INSERT_ONLY);
+        outputBuffer = new TypedFluidBuffer(this, 4 * FluidConstants.BUCKET, fluidVariant -> fluidVariant.isOf(FluidInitialiser.STILL_BLOOD), TypedFluidBuffer.Mode.EXTRACT_ONLY);
+        buffer = new MultiTypedFluidBuffer(this, List.of(inputBuffer, outputBuffer));
     }
 
     @Override
@@ -33,5 +41,33 @@ public abstract class BloodMachineBlockEntity<T extends BloodMachineBlockEntity>
     public void setNeedsUpdate(boolean needsUpdate)
     {
 
+    }
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt)
+    {
+        super.writeNbt(nbt);
+        buffer.writeNbt(nbt);
+        return nbt;
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt)
+    {
+        super.readNbt(nbt);
+        buffer.readNbt(nbt);
+    }
+
+    @Override
+    public NbtCompound toClientTag(NbtCompound tag)
+    {
+        writeNbt(tag);
+        return tag;
+    }
+
+    @Override
+    public void fromClientTag(NbtCompound nbt)
+    {
+        readNbt(nbt);
     }
 }
--- src/main/java/com/neep/neepmeat/mixin/FurnaceAccessor.java
+++ src/main/java/com/neep/neepmeat/mixin/FurnaceAccessor.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
-public interface FurnaceMixin
+public interface FurnaceAccessor
 {
 	@Accessor
 	int getBurnTime();
