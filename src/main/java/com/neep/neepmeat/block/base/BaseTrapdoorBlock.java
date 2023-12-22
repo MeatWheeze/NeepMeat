d7390eb5bd3b1f393b2b38625f46c3d496133851
MeatWheeze <ooo@oo.com>
2023-12-22 18:45:54 +0000

    Added rusty trapdoor

---
+++ src/main/java/com/neep/neepmeat/block/base/BaseTrapdoorBlock.java
@@ -0,0 +1,29 @@
+package com.neep.neepmeat.block.base;
+
+import com.neep.neepmeat.item.BaseBlockItem;
+import net.minecraft.block.TrapdoorBlock;
+import net.minecraft.item.BlockItem;
+
+public class BaseTrapdoorBlock extends TrapdoorBlock implements NMBlock
+{
+    public BaseBlockItem blockItem;
+    private final String registryName;
+
+    public BaseTrapdoorBlock(String itemName, int itemMaxStack, boolean hasLore, Settings settings)
+    {
+        super(settings);
+        this.blockItem = new BaseBlockItem(this, itemName, itemMaxStack, hasLore);
+        this.registryName = itemName;
+    }
+
+    public BlockItem getBlockItem()
+    {
+        return blockItem;
+    }
+
+    @Override
+    public String getRegistryName()
+    {
+        return registryName;
+    }
+}
