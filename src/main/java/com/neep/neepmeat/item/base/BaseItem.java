ef711fa569c24b4372d03b0faa28e1851b1e9127
MeatWheeze <ooo@oo.com>
2023-12-22 18:45:56 +0000

    Added lore option on base item

--- src/main/java/com/neep/neepmeat/item/base/BaseItem.java
+++ src/main/java/com/neep/neepmeat/item/base/BaseItem.java
@@ -1,16 +1,27 @@
 package com.neep.neepmeat.item.base;
 
+import com.neep.neepmeat.NeepMeat;
 import com.neep.neepmeat.item.NMItem;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BaseItem extends Item implements NMItem
 {
     private final String registryName;
+    private final boolean hasLore;
 
-    public BaseItem(String registryName, Settings settings)
+    public BaseItem(String registryName, boolean hasLore, Settings settings)
     {
         super(settings);
         this.registryName = registryName;
+        this.hasLore = hasLore;
     }
 
     @Override
@@ -18,4 +29,13 @@ public class BaseItem extends Item implements NMItem
     {
         return registryName;
     }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        if (hasLore)
+        {
+            tooltip.add(new TranslatableText("item." + NeepMeat.NAMESPACE + "." + getRegistryName() + ".lore").formatted(Formatting.GRAY));
+        }
+    }
+
 }
