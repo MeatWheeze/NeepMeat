3e14e16316192364a831ef15f7e0fd4e2ef2dcf4
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:03 +0000

    Motor and Linear Oscillator work together

--- src/main/java/com/neep/neepmeat/block/machine/MotorBlock.java
+++ src/main/java/com/neep/neepmeat/block/machine/MotorBlock.java
@@ -1,13 +1,36 @@
 package com.neep.neepmeat.block.machine;
 
 import com.neep.meatlib.block.BaseFacingBlock;
+import com.neep.neepmeat.blockentity.machine.MotorBlockEntity;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class MotorBlock extends BaseFacingBlock
+public class MotorBlock extends BaseFacingBlock implements BlockEntityProvider
 {
     public MotorBlock(String itemName, int itemMaxStack, boolean hasLore, Settings settings)
     {
         super(itemName, itemMaxStack, hasLore, settings);
     }
 
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit)
+    {
+        System.out.println(world.getBlockEntity(pos));
+        return ActionResult.SUCCESS;
+    }
 
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state)
+    {
+        return new MotorBlockEntity(pos, state);
+    }
 }
