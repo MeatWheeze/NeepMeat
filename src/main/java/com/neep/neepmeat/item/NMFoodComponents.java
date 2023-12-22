6436c5d653346096daa745d8cd338d6ed496d2b7
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:10 +0000

    feat: Make Enlightened Brain edible

--- src/main/java/com/neep/neepmeat/item/NMFoodComponents.java
+++ src/main/java/com/neep/neepmeat/item/NMFoodComponents.java
@@ -6,6 +6,7 @@ import net.minecraft.item.FoodComponent;
 
 public class NMFoodComponents
 {
+    public static final FoodComponent ENLIGHTENED_BRAIN = new FoodComponent.Builder().hunger(6).saturationModifier(0.6f).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 600, 1), 1).meat().build();
     public static final FoodComponent MEAT_SCRAP = new FoodComponent.Builder().hunger(1).saturationModifier(0.3f).meat().build();
     public static final FoodComponent MEAT_BRICK = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600, 0), 0.7f).meat().build();
     public static final FoodComponent COOKED_MEAT_BRICK = new FoodComponent.Builder().hunger(10).saturationModifier(0.8f).meat().build();
