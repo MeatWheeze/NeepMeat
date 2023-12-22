48e934cce65862cbbcc265c118feadd2d1859dbf
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:02 +0000

    Improved rendering of Tank Minecart

--- src/main/java/com/neep/neepmeat/client/renderer/TankMinecartRenderer.java
+++ src/main/java/com/neep/neepmeat/client/renderer/TankMinecartRenderer.java
@@ -1,13 +1,14 @@
 package com.neep.neepmeat.client.renderer;
 
 import com.neep.neepmeat.entity.TankMinecartEntity;
-import com.neep.neepmeat.init.NMBlocks;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MinecartEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3f;
 
 public class TankMinecartRenderer extends MinecartEntityRenderer<TankMinecartEntity>
 {
@@ -20,14 +21,16 @@ public class TankMinecartRenderer extends MinecartEntityRenderer<TankMinecartEnt
     public void render(TankMinecartEntity minecart, float f, float g, MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int i)
     {
         super.render(minecart, f, g, matrices, vertexConsumerProvider, i);
-
-//        matrices.push();
-//        float p = 0.75f;
-//        matrices.scale(0.75f, 0.75f, 0.75f);
-//        matrices.translate(-0.5, (float)(minecart.getBlockOffset() - 8) / 16.0f, 0.5);
-//        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(90.0f));
-//        renderBlock(minecart, f, NMBlocks.TANK.getDefaultState(), matrices, vertexConsumerProvider, i);
-//
-//        matrices.pop();
     }
+
+    @Override
+    protected void renderBlock(TankMinecartEntity entity, float delta, BlockState state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light)
+    {
+        super.renderBlock(entity, delta, state, matrices, vertexConsumers, light);
+        SingleVariantStorage<FluidVariant> storage = entity.getBuffer(null);
+        FluidVariant fluid = storage.getResource();
+        float scale = storage.getAmount() / (float) storage.getCapacity();
+        IntegratorEggRenderer.renderFluidCuboid(vertexConsumers, matrices, fluid, 0.1f, 0.1f, 0.9f, 0.9f, scale);
+    }
+
 }
