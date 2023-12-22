8fc3893125b907668a7059db5f5b6a32d2ae6e23
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:05 +0000

    Added Data Cable block

---
+++ src/main/java/com/neep/neepmeat/block/DataCableBlock.java
@@ -0,0 +1,55 @@
+package com.neep.neepmeat.block;
+
+import com.neep.meatlib.item.BaseBlockItem;
+import com.neep.neepmeat.block.item_transport.PneumaticTubeBlock;
+import com.neep.neepmeat.block.pipe.IItemPipe;
+import com.neep.neepmeat.fluid_transfer.PipeConnectionType;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+public class DataCableBlock extends AbstractPipeBlock
+{
+    public DataCableBlock(String itemName, int itemMaxStack, boolean hasLore, Settings settings)
+    {
+        super(itemName, itemMaxStack, hasLore, BaseBlockItem::new, settings);
+    }
+
+    @Override
+    public boolean canConnectTo(BlockState state, Direction direction, World world, BlockPos pos)
+    {
+        return state.isOf(this);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos)
+    {
+        PipeConnectionType type = state.get(DIR_TO_CONNECTION.get(direction));
+        boolean forced = type == PipeConnectionType.FORCED;
+        boolean otherConnected = false;
+
+        boolean canConnect = canConnectTo(neighborState, direction.getOpposite(), (World) world, neighborPos);
+//        if (!world.isClient() && !(neighborState.getBlock() instanceof IItemPipe))
+//        {
+//            canConnect = canConnect || (canConnectApi((World) world, pos, state, direction));
+//        }
+
+        // Check if neighbour is forced
+        if (neighborState.getBlock() instanceof PneumaticTubeBlock)
+        {
+            forced = forced || neighborState.get(DIR_TO_CONNECTION.get(direction.getOpposite())) == PipeConnectionType.FORCED;
+            otherConnected = neighborState.get(DIR_TO_CONNECTION.get(direction.getOpposite())) == PipeConnectionType.SIDE;
+
+        }
+
+        // AAAAAAAAAAAAAAAAAAAA
+        PipeConnectionType finalConnection =
+                otherConnected ? PipeConnectionType.SIDE :
+                        forced ? PipeConnectionType.FORCED
+                                : canConnect ? PipeConnectionType.SIDE : PipeConnectionType.NONE;
+
+        return state.with(DIR_TO_CONNECTION.get(direction), finalConnection);
+    }
+}
