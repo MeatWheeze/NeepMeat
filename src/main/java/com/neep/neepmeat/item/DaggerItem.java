773927bf01600e76a9d7fb6e31c39ba55d656e8d
MeatWheeze <ooo@oo.com>
2023-12-22 18:46:35 +0000

    Tweaked behaviour of Sacrificial Scalpel

--- src/main/java/com/neep/neepmeat/item/DaggerItem.java
+++ src/main/java/com/neep/neepmeat/item/DaggerItem.java
@@ -2,11 +2,14 @@ package com.neep.neepmeat.item;
 
 import com.neep.meatlib.item.BaseSwordItem;
 import com.neep.meatweapons.Util;
+import com.neep.neepmeat.init.NMBlocks;
 import com.neep.neepmeat.init.NMFluids;
 import com.neep.neepmeat.init.NMItems;
 import com.neep.neepmeat.transport.block.fluid_transport.entity.FluidDrainBlockEntity;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
@@ -19,16 +22,20 @@ import net.minecraft.item.ToolMaterials;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockStateRaycastContext;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -65,6 +72,16 @@ public class DaggerItem extends BaseSwordItem
         return true;
     }
 
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner)
+    {
+        if (state.isOf(NMBlocks.INTEGRATOR_EGG))
+        {
+
+        }
+        return super.postMine(stack, world, state, pos, miner);
+    }
+
     public static void spawnBloodParticles(Vec3d entityPos, ServerWorld world)
     {
 //        world.spawnParticles(new BlockStateParticleEffect(ParticleTypes.BLOCK, NMFluids.BLOOD.getDefaultState()),
@@ -84,47 +101,68 @@ public class DaggerItem extends BaseSwordItem
     @Override
     public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks)
     {
-        if (!world.isClient)
+        if (world.isClient())
         {
+            if (remainingUseTicks % 10 == 0)
+            {
+                user.playSound(SoundEvents.ENTITY_SHEEP_SHEAR, 1, 1);
+                user.playSound(SoundEvents.ITEM_HONEYCOMB_WAX_ON, 1, 1);
+            }
         }
     }
 
     @Override
     public int getMaxUseTime(ItemStack stack)
     {
-        return 100000;
+        return 40;
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand)
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user)
     {
-        user.setCurrentHand(hand);
-        ItemStack itemStack = user.getStackInHand(hand);
-        return TypedActionResult.consume(itemStack);
-    }
-
-    @Override
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks)
-    {
-        if (user instanceof PlayerEntity player && !world.isClient() && this.getMaxUseTime(stack) - remainingUseTicks > 10)
+        if (user instanceof PlayerEntity player && !world.isClient())
         {
-            Vec3d pos = user.getEyePos();
-            Vec3d end = pos.add(user.getRotationVec(0.5f).multiply(20));
-            Optional<LivingEntity> optional = getTarget(player, user.getEyePos(), end, 2);
+            Optional<LivingEntity> optional = getEntity(player);
+            BlockPos pos = getBlock(player);
             if (optional.isPresent())
             {
                 LivingEntity entity = optional.get();
-                entity.damage(DamageSource.player(player), 3);
-//                entity.damage(user.getDamageSources().playerAttack(player), 3);
+                entity.damage(DamageSource.player(player), 5);
+
                 if (entity.getHealth() <= MAX_HEALTH)
                 {
                     spawnSpecialDrop(world, entity.getPos(), entity);
                     entity.kill();
                 }
             }
-            return;
+            else if (pos != null && world.getBlockState(pos).isOf(NMBlocks.INTEGRATOR_EGG))
+            {
+                world.breakBlock(pos, false);
+                ItemStack stack1 = NMItems.CHRYSALIS.getDefaultStack();
+                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack1);
+                world.spawnEntity(itemEntity);
+            }
+            return stack;
         }
-        PlayerEntity playerEntity = (PlayerEntity) user;
+        return super.finishUsing(stack, world, user);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand)
+    {
+        ItemStack itemStack = user.getStackInHand(hand);
+        if (getEntity(user).isPresent() || getBlock(user) != null)
+        {
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(itemStack);
+        }
+        return TypedActionResult.fail(itemStack);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks)
+    {
+        super.onStoppedUsing(stack, world, user, remainingUseTicks);
     }
 
     public static void spawnSpecialDrop(World world, Vec3d pos, LivingEntity entity)
@@ -134,6 +172,13 @@ public class DaggerItem extends BaseSwordItem
             ItemEntity item = new ItemEntity(world, pos.x, pos.y, pos.z, NMItems.REANIMATED_HEART.getDefaultStack());
             world.spawnEntity(item);
         }
+    }
+
+    protected Optional<LivingEntity> getEntity(PlayerEntity user)
+    {
+        Vec3d pos = user.getEyePos();
+        Vec3d end = pos.add(user.getRotationVec(0.5f).multiply(20));
+        return getTarget(user, user.getEyePos(), end, 4);
     }
 
     public Optional<LivingEntity> getTarget(PlayerEntity caster, Vec3d start, Vec3d end, double distance)
@@ -163,14 +208,25 @@ public class DaggerItem extends BaseSwordItem
         return Optional.empty();
     }
 
-//    @Override
-//    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand)
-//    {
-//        if (entity instanceof ZombieEntity)
-//        {
-//            System.out.println("ooooooo");
-//            return ActionResult.SUCCESS;
-//        }
-//        return ActionResult.FAIL;
-//    }
+    @Nullable
+    protected BlockPos getBlock(PlayerEntity user)
+    {
+        Vec3d pos = user.getEyePos();
+        Vec3d end = pos.add(user.getRotationVec(0.5f).multiply(4));
+        return getBlockTarget(pos, end, user.getWorld(), user);
+    }
+
+    @Nullable
+    protected BlockPos getBlockTarget(Vec3d start, Vec3d end, World world, Entity entity)
+    {
+        RaycastContext context = new RaycastContext(start, end, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, entity);
+        var result = world.raycast(context);
+
+        // Jank!
+        if (result.getType() != HitResult.Type.MISS && world.getBlockState(result.getBlockPos()).isOf(NMBlocks.INTEGRATOR_EGG))
+        {
+            return result.getBlockPos();
+        }
+        return null;
+    }
 }
