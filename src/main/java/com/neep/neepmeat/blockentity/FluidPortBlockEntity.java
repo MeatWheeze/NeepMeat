8b27f7b195a385ee9db7096754fa697bbf2678f7
MeatWheeze <ooo@oo.com>
2023-12-22 18:45:55 +0000

    Fluid port now acts as an interface

---
+++ src/main/java/com/neep/neepmeat/blockentity/FluidPortBlockEntity.java
@@ -0,0 +1,113 @@
+package com.neep.neepmeat.blockentity;
+
+import com.neep.neepmeat.block.FluidPortBlock;
+import com.neep.neepmeat.block.ItemDuctBlock;
+import com.neep.neepmeat.fluid_util.FluidBuffer;
+import com.neep.neepmeat.init.BlockEntityInitialiser;
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiCache;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+@SuppressWarnings("UnstableApiUsage")
+public class FluidPortBlockEntity extends BlockEntity implements Storage<FluidVariant>
+{
+    protected BlockApiCache<Storage<FluidVariant>, Direction> cache;
+
+    public FluidPortBlockEntity(BlockEntityType type, BlockPos pos, BlockState state)
+    {
+        super(type, pos, state);
+    }
+
+    public FluidPortBlockEntity(BlockPos pos, BlockState state)
+    {
+        super(BlockEntityInitialiser.FLUID_PORT, pos, state);
+    }
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound tag)
+    {
+        super.writeNbt(tag);
+        return tag;
+    }
+
+    @Override
+    public void readNbt(NbtCompound tag)
+    {
+        super.readNbt(tag);
+    }
+
+    @Override
+    public long insert(FluidVariant resource, long maxAmount, TransactionContext transaction)
+    {
+        if (cache == null)
+        {
+            updateApiCache(getPos(), getCachedState());
+        }
+        Storage<FluidVariant> storage = cache.find(getCachedState().get(FluidPortBlock.FACING));
+        if (storage != null)
+        {
+            return storage.insert(resource, maxAmount, transaction);
+        }
+        return 0;
+    }
+
+    @Override
+    public long extract(FluidVariant resource, long maxAmount, TransactionContext transaction)
+    {
+        if (cache == null)
+        {
+            updateApiCache(getPos(), getCachedState());
+        }
+        Storage<FluidVariant> storage = cache.find(getCachedState().get(FluidPortBlock.FACING));
+        if (storage != null)
+        {
+            return storage.extract(resource, maxAmount, transaction);
+        }
+        return 0;
+    }
+
+    @Override
+    public Iterator<StorageView<FluidVariant>> iterator(TransactionContext transaction)
+    {
+        if (cache == null)
+        {
+            updateApiCache(getPos(), getCachedState());
+        }
+        Storage<FluidVariant> storage = cache.find(getCachedState().get(FluidPortBlock.FACING));
+        if (storage != null)
+        {
+            return storage.iterator(transaction);
+        }
+        return Collections.emptyIterator();
+    }
+
+    public void updateApiCache(BlockPos pos, BlockState state)
+    {
+        if (getWorld() == null || !(getWorld() instanceof ServerWorld))
+            return;
+
+        Direction direction = state.get(FluidPortBlock.FACING);
+        cache = BlockApiCache.create(FluidStorage.SIDED, (ServerWorld) getWorld(), pos.offset(direction));
+    }
+
+    public boolean hasCache()
+    {
+        return cache != null;
+    }
+}
