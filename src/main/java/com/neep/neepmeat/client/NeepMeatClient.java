b93e0f123f11e07f377f92a3c8838570800ca6c8
MeatWheeze <ooo@oo.com>
2024-05-15 15:52:19 +0100

    Added recipe for Rider Control Rail

--- src/main/java/com/neep/neepmeat/client/NeepMeatClient.java
+++ src/main/java/com/neep/neepmeat/client/NeepMeatClient.java
@@ -332,6 +332,7 @@ public class NeepMeatClient implements ClientModInitializer
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), FluidTransport.WINDOW_PIPE);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), NMBlocks.HOLDING_TRACK);
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), NMBlocks.CONTROL_TRACK);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), NMBlocks.LARGE_FAN);
     }
