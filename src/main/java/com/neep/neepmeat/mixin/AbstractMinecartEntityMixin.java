e44ba3d946036703a30d2a49be860398c357d757
MeatWheeze <meatwheeze@proton.me>
2024-06-06 19:32:10 +0100

    Fixed rail incompatibility with Audaki Cart Engine

--- src/main/java/com/neep/neepmeat/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/com/neep/neepmeat/mixin/AbstractMinecartEntityMixin.java
@@ -1,8 +1,6 @@
 package com.neep.neepmeat.mixin;
 
-import com.neep.neepmeat.block.DumpingTrackBlock;
 import com.neep.neepmeat.block.SpecialRail;
-import com.neep.neepmeat.init.NMBlocks;
 import com.neep.neepmeat.interfaces.AbstractMinecartEntityAccess;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -11,20 +9,26 @@ import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractMinecartEntity.class)
-public class AbstractMinecartEntityMixin implements AbstractMinecartEntityAccess
+public abstract class AbstractMinecartEntityMixin implements AbstractMinecartEntityAccess
 {
+    @Shadow protected abstract boolean willHitBlockAt(BlockPos pos);
+
     @Unique
     private Vec3d controllerVelocity = Vec3d.ZERO;
 
     // Passenger velocity is reset at some point after this is called.
-    @Inject(method = "moveOnRail", at = @At("HEAD"))
-    void onMoveOnRail(BlockPos pos, BlockState state, CallbackInfo ci)
+    // We have to inject outside moveOnRail for compatibility with Audaki Cart Engine, which cancels moveOnRail immediately.
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;moveOnRail(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V"),
+        locals = LocalCapture.CAPTURE_FAILSOFT)
+    void tick(CallbackInfo ci, int i, int j, int k, BlockPos blockPos, BlockState blockState)
     {
         AbstractMinecartEntity thisEntity = ((AbstractMinecartEntity) (Object) (this));
         Entity passenger = thisEntity.getFirstPassenger();
@@ -33,9 +37,9 @@ public class AbstractMinecartEntityMixin implements AbstractMinecartEntityAccess
             controllerVelocity = player.getVelocity();
         }
 
-        if (state.getBlock() instanceof SpecialRail specialRail)
+        if (blockState.getBlock() instanceof SpecialRail specialRail)
         {
-            specialRail.apply(thisEntity.getWorld(), pos, state, thisEntity);
+            specialRail.apply(thisEntity.getWorld(), blockPos, blockState, thisEntity);
         }
     }
 
