0785143e965ea074ec176cf8b63ebc8eb5fb3bf6
MeatWheeze <meatwheeze@proton.me>
2024-07-18 09:31:16 +0100

    meatgun: Disabled controls for aux slot in BaseStaffModule

--- src/main/java/com/neep/meatweapons/meatgun/module/BaseStaffModule.java
+++ src/main/java/com/neep/meatweapons/meatgun/module/BaseStaffModule.java
@@ -1,18 +1,24 @@
 package com.neep.meatweapons.meatgun.module;
 
 import com.neep.meatweapons.meatgun.RootModuleHolder;
+import com.neep.meatweapons.network.MWAttackC2SPacket;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
 import org.joml.Matrix4f;
 
 import java.util.List;
 
 public class BaseStaffModule extends AbstractMeatgunModule
 {
+    private final SimpleModuleSlot mainSlot;
+
     public BaseStaffModule(RootModuleHolder.Listener listener)
     {
         super(listener);
 
-        var mainSlot = new SimpleModuleSlot(listener, new Matrix4f()
+        mainSlot = new SimpleModuleSlot(listener, new Matrix4f()
                 .rotateX((float) Math.toRadians(90))
                 .translate(0, 0, -8 / 16f));
 
@@ -23,6 +29,24 @@ public class BaseStaffModule extends AbstractMeatgunModule
                 .translate(0, 4 / 16f, -1 / 16f));
 
         setSlots(List.of(mainSlot, auxSlot));
+    }
+
+    @Override
+    public void trigger(World world, PlayerEntity player, ItemStack stack, int id, double pitch, double yaw, MWAttackC2SPacket.HandType handType)
+    {
+        mainSlot.get().trigger(world, player, stack, id, pitch, yaw, handType);
+    }
+
+    @Override
+    public void release(World world, PlayerEntity player, ItemStack stack, int id, double pitch, double yaw, MWAttackC2SPacket.HandType handType)
+    {
+        mainSlot.get().release(world, player, stack, id, pitch, yaw, handType);
+    }
+
+    @Override
+    public void tickTrigger(World world, PlayerEntity player, ItemStack stack, int id, double pitch, double yaw, MWAttackC2SPacket.HandType handType)
+    {
+        mainSlot.get().tickTrigger(world, player, stack, id, pitch, yaw, handType);
     }
 
     @Override
