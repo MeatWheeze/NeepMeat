dad4737497fe99d78783cd32a1693297426d20d1
MeatWheeze <meatwheeze@proton.me>
2024-07-24 20:12:17 +0100

    meatgun: Reduced bloodthrower firing rate

--- src/main/java/com/neep/meatweapons/meatgun/module/BloodthrowerModule.java
+++ src/main/java/com/neep/meatweapons/meatgun/module/BloodthrowerModule.java
@@ -41,7 +41,7 @@ public class BloodthrowerModule extends ShooterModule
 
     public BloodthrowerModule(RootModuleHolder.Listener listener)
     {
-        super(listener, 1, 1, AmmunitionType.BLOOD);
+        super(listener, 1, 2, AmmunitionType.BLOOD);
     }
 
     public BloodthrowerModule(RootModuleHolder.Listener listener, NbtCompound nbt)
